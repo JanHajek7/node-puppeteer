fb7160af224867d2fe6a82727d94df144849bf83
Furát Ismail <furat.ismail@cadcom.cz>
2022-11-21 20:31:22 +0100

    trycatch

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
 import puppeteer from "puppeteer";
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false }); // default is true
-
-  const delay = (milliseconds) =>
-    new Promise((r) => setTimeout(r, milliseconds));
-
-  let index = 1;
-  for (const item of MockData) {
-    const page = await browser.newPage();
-    await page.goto("http://localhost:4200/");
-
-    await page.type("input[name=name]", item.name);
-    await delay(1500);
-
-    await page.type("input[name=email]", item.email);
-    await delay(1500);
-
-    await page.type("input[name=confirm]", item.confirm);
-    await delay(1500);
-
-    await page.evaluate(() => {
-      const button = document.querySelectorAll("button[type='submit']")[0];
-      button.click();
-    });
-    await delay(1500);
-
-    await page.screenshot({ path: `tmp/screenshots/form_${index}.png` });
-    // await page.screenshot({ path: `tmp/pdfs/form_${index}.pdf` });
-
-    index++;
-  }
-
-  await browser.close();
-
-})();
-
 export const MockData = [
   {
     name: "Jennifer Bullock",
@@ -53,3 +17,43 @@ export const MockData = [
     confirm: "eu.elit@icloud.org",
   },
 ];
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false }); // default is true
+
+  try {
+
+    const delay = (milliseconds) =>
+      new Promise((r) => setTimeout(r, milliseconds));
+
+    let index = 1;
+    for (const item of MockData) {
+      const page = await browser.newPage();
+      await page.goto("http://localhost:4200/");
+
+      await page.type("input[name=name]", item.name);
+      await delay(1500);
+
+      await page.type("input[name=email]", item.email);
+      await delay(1500);
+
+      await page.type("input[name=confirm]", item.confirm);
+      await delay(1500);
+
+      await page.evaluate(() => {
+        const button = document.querySelectorAll("button[type='submit']")[0];
+        button.click();
+      });
+      await delay(1500);
+
+      await page.screenshot({ path: `tmp/screenshots/form_${index}.png` });
+      // await page.screenshot({ path: `tmp/pdfs/form_${index}.pdf` });
+
+      index++;
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await browser.close();
+  }
+})();
